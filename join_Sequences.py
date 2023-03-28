1a4d106ebf569dd17fd6f3bead56b71f9d3f3f33
AmirMahfouz <101039892+AmirMahfouz@users.noreply.github.com>
2023-03-28 20:35:40 +0200

    Add files via upload

---
+++ join_Sequences.py
@@ -0,0 +1,8 @@
+Seq1 = input("Enter Your First Sequence :")
+Seq2 = input("Enter Your second Sequence :")
+Seq3 = input("Enter Your Third Sequence :")
+
+print("".join([Seq1 , Seq2, Seq3]))
+
+
+input( "press Enter to EXIT.. !")
